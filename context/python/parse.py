ed6024aeb4c43031c885b192fb84a97ab3f88e21
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-07-17 15:41:02 -0400

    parse.py can handle input.json

--- context/python/parse.py
+++ context/python/parse.py
@@ -7,6 +7,25 @@ import pathlib
 from sets import Sets
 
 
+def arg_parser():
+    parser = argparse.ArgumentParser(
+        description='A variety of set intersection visualizations')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        '--json', type=str,
+        help='If given file does not exist, fall back to environment variable')
+    group.add_argument(
+        '--lists', nargs='+', type=argparse.FileType('r'),
+        help='Single column lists')
+
+    parser.add_argument(
+        '--output', type=str,
+        help='Destination directory')
+
+    return parser
+
+
 def get_input_json(possible_input_file):
     '''
     Checks three possible sources for JSON input,
@@ -27,42 +46,33 @@ def get_input_json(possible_input_file):
 
     return None
 
-def load(input_json_path):
+
+def read_json(input_json_path):
     input_json = get_input_json(input_json_path)
-    if input_json:
-        data = json.loads(input_json)
-        strings = [requests.get(url).text for url in data['file_relationships']]
-        sets = []
+    data = json.loads(input_json)
+    return {
+        os.path.basename(url): {
+            l.strip() for l in requests.get(url).text.split('\n')
+        } for url in data['file_relationships']
+    }
 
-def arg_parser():
-    parser = argparse.ArgumentParser(
-        description='A variety of set intersection visualizations')
-
-    parser.add_argument(
-        '--json', type=argparse.FileType('r'),
-        help='input.json')
-    parser.add_argument(
-        '--lists', nargs='+', type=argparse.FileType('r'),
-        help='Single column lists')
-    parser.add_argument(
-        '--output', type=str,
-        help='Destination directory'
-    )
-
-    return parser
 
 def read_lists(lists):
-    return {os.path.basename(f.name) : {l.strip() for l in f.readlines()} for f in lists}
+    return {os.path.basename(f.name) : {l.strip() for l in f.readlines()}
+            for f in lists}
+
 
 if __name__ == '__main__':
     parser = arg_parser()
     args = parser.parse_args()
     if args.json:
-        raise StandardError('TODO')
+        lists = read_json(args.json)
     elif args.lists:
         lists = read_lists(args.lists)
-        sets = Sets(lists)
-        pathlib.Path(args.output).mkdir(parents=True, exist_ok=True)
-        sets.print_columns(os.path.join(args.output, 'columns.txt'))
-        sets.print_ratio_matrix(os.path.join(args.output, 'ratio_matrix.txt'))
+    else:
+        raise Exception('Either --json or --lists should be given')
+    sets = Sets(lists)
+    pathlib.Path(args.output).mkdir(parents=True, exist_ok=True)
+    sets.print_columns(os.path.join(args.output, 'columns.txt'))
+    sets.print_ratio_matrix(os.path.join(args.output, 'ratio_matrix.txt'))
 
